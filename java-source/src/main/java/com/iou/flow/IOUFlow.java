099db7f7bbf667f5b273e605cca4b07765944549
Joel Dudley <joel.dudley@r3cev.com>
2017-02-13 15:47:55 +0000

    Reduces required flow boilerplate.

--- java-source/src/main/java/com/iou/flow/IOUFlow.java
+++ java-source/src/main/java/com/iou/flow/IOUFlow.java
@@ -1,21 +1,33 @@
 package com.iou.flow;
 
 import co.paralleluniverse.fibers.Suspendable;
+import com.iou.state.IOUState;
 import net.corda.core.crypto.Party;
 import net.corda.core.flows.FlowLogic;
+import net.corda.core.transactions.SignedTransaction;
 
 public class IOUFlow {
-    public static class Initiator {
+    public static class Initiator extends FlowLogic<SignedTransaction> {
+        private final IOUState iou;
+        private final Party otherParty;
+
+        public Initiator(IOUState iou, Party otherParty) {
+            this.iou = iou;
+            this.otherParty = otherParty;
+        }
 
         @Suspendable
-        public Void call() {
+        public SignedTransaction call() {
             return null;
         }
     }
 
     public static class Acceptor extends FlowLogic<Void> {
+        private final Party otherParty;
 
-        public Acceptor(Party otherParty) {}
+        public Acceptor(Party otherParty) {
+            this.otherParty = otherParty;
+        }
 
         @Suspendable
         @Override
