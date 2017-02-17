d789e3ac35e76ae87df32a6a63a5c18f5eec7a51
Joel Dudley <joel.dudley@r3cev.com>
2017-02-17 18:07:09 +0000

    Provides the state and contract boilerplate from the get-go.

--- java-source/src/main/java/com/iou/state/IOUState.java
+++ java-source/src/main/java/com/iou/state/IOUState.java
@@ -13,4 +13,55 @@ import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
-public class IOUState {}
+/**
+ * The state object recording IOU agreements between two parties.
+ *
+ * A state must implement [ContractState] or one of its descendants.
+ *
+ * @param iouValue details of the IOU.
+ * @param sender the party issuing the IOU.
+ * @param recipient the party receiving and approving the IOU.
+ * @param contract the contract which governs which transactions are valid for this state object.
+ */
+public class IOUState implements LinearState {
+    private final Integer iouValue;
+    private final Party sender;
+    private final Party recipient;
+    private final IOUContract contract;
+    private final UniqueIdentifier linearId;
+
+    public IOUState(Integer iouValue,
+                    Party sender,
+                    Party recipient,
+                    IOUContract contract)
+    {
+        this.iouValue = iouValue;
+        this.sender = sender;
+        this.recipient = recipient;
+        this.contract = contract;
+        this.linearId = new UniqueIdentifier();
+    }
+
+    public Integer getIOUValue() { return iouValue; }
+    public Party getSender() { return sender; }
+    public Party getRecipient() { return recipient; }
+    @Override public IOUContract getContract() { return contract; }
+    @Override public UniqueIdentifier getLinearId() { return linearId; }
+    @Override public List<CompositeKey> getParticipants() {
+        return Stream.of(sender, recipient)
+                .map(Party::getOwningKey)
+                .collect(toList());
+    }
+
+    /**
+     * This returns true if the state should be tracked by the vault of a particular node. In this case the logic is
+     * simple; track this state if we are one of the involved parties.
+     */
+    @Override public boolean isRelevant(Set<? extends PublicKey> ourKeys) {
+        final List<PublicKey> partyKeys = Stream.of(sender, recipient)
+                .flatMap(party -> party.getOwningKey().getKeys().stream())
+                .collect(toList());
+        return ourKeys.stream().anyMatch(partyKeys::contains);
+
+    }
+}
