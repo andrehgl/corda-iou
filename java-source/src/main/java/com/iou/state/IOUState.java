7e97de6804b93526812b396829cba3cef6883e6e
Joel Dudley <joel.dudley@r3cev.com>
2017-03-09 16:46:09 +0000

    Refactors state back to minimal ContractState.

--- java-source/src/main/java/com/iou/state/IOUState.java
+++ java-source/src/main/java/com/iou/state/IOUState.java
@@ -1,16 +1,15 @@
 package com.iou.state;
 
 import com.iou.contract.IOUContract;
-import net.corda.core.contracts.LinearState;
-import net.corda.core.contracts.UniqueIdentifier;
+import kotlin.collections.EmptyList;
+import net.corda.core.contracts.ContractState;
 import net.corda.core.crypto.CompositeKey;
 import net.corda.core.crypto.Party;
 
-import java.security.PublicKey;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Stream;
 
+import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.toList;
 
 /**
@@ -18,50 +17,17 @@ import static java.util.stream.Collectors.toList;
  *
  * A state must implement [ContractState] or one of its descendants.
  *
- * @param iouValue details of the IOU.
- * @param sender the party issuing the IOU.
- * @param recipient the party receiving and approving the IOU.
- * @param contract the contract which governs which transactions are valid for this state object.
+ *  * @param contract the contract which governs which transactions are valid for this state object.
  */
-public class IOUState implements LinearState {
-    private final Integer iouValue;
-    private final Party sender;
-    private final Party recipient;
+public class IOUState implements ContractState {
     private final IOUContract contract;
-    private final UniqueIdentifier linearId;
 
-    public IOUState(Integer iouValue,
-                    Party sender,
-                    Party recipient,
-                    IOUContract contract)
-    {
-        this.iouValue = iouValue;
-        this.sender = sender;
-        this.recipient = recipient;
+    public IOUState(IOUContract contract) {
         this.contract = contract;
-        this.linearId = new UniqueIdentifier();
     }
-
-    public Integer getIOUValue() { return iouValue; }
-    public Party getSender() { return sender; }
-    public Party getRecipient() { return recipient; }
+    
     @Override public IOUContract getContract() { return contract; }
-    @Override public UniqueIdentifier getLinearId() { return linearId; }
     @Override public List<CompositeKey> getParticipants() {
-        return Stream.of(sender, recipient)
-                .map(Party::getOwningKey)
-                .collect(toList());
-    }
-
-    /**
-     * This returns true if the state should be tracked by the vault of a particular node. In this case the logic is
-     * simple; track this state if we are one of the involved parties.
-     */
-    @Override public boolean isRelevant(Set<? extends PublicKey> ourKeys) {
-        final List<PublicKey> partyKeys = Stream.of(sender, recipient)
-                .flatMap(party -> party.getOwningKey().getKeys().stream())
-                .collect(toList());
-        return ourKeys.stream().anyMatch(partyKeys::contains);
-
+        return emptyList();
     }
 }
