680312ff23c43c45ad01a22dfe198e2dab98dafd
Tim Blankers <mail@timblankers.nl>
2017-04-04 15:39:36 +0100

    Implement initial IOU state

--- kotlin-source/src/main/kotlin/com/iou/kotlin/state/IOUState.kt
+++ kotlin-source/src/main/kotlin/com/iou/kotlin/state/IOUState.kt
@@ -15,7 +15,17 @@ import java.security.PublicKey
  *
  * @param contract the contract which governs which transactions are valid for this state object.
  */
-data class IOUState(override val contract: IOUContract) : ContractState {
+data class IOUState(
+        val iouValue: Int,
+        val sender: Party,
+        val recipient: Party,
+        override val contract: IOUContract
+        ) : LinearState {
 
-    override val participants: List<CompositeKey> get() = listOf()
+    override val participants: List<CompositeKey> get() = listOf(sender.owningKey, recipient.owningKey)
+    override val linearId: UniqueIdentifier = UniqueIdentifier()
+    override fun isRelevant(ourKeys: Set<PublicKey>): Boolean {
+//        return ourKeys.contains(participants[0].singleKey) || ourKeys.contains(participants[1].singleKey)
+        return ourKeys.intersect(participants.flatMap { it.keys }).isNotEmpty()
+    }
 }
--- kotlin-source/src/test/kotlin/com/iou/kotlin/states/IOUStateTests.kt
+++ kotlin-source/src/test/kotlin/com/iou/kotlin/states/IOUStateTests.kt
@@ -18,56 +18,56 @@ class IOUStateTests {
     val miniCorpKey = MINI_CORP_PUBKEY
     val megaCorpKey = MEGA_CORP_PUBKEY
 
-//    @Test
-//    fun hasIOUValueFieldOfCorrectType() {
-//        // Does the iouValue field exist?
-//        IOUState::class.java.getDeclaredField("iouValue")
-//        // Is the iouValue field of the correct type?
-//        assertEquals(IOUState::class.java.getDeclaredField("iouValue").type, Int::class.java)
-//    }
-//
-//    @Test
-//    fun hasSenderFieldOfCorrectType() {
-//        // Does the sender field exist?
-//        IOUState::class.java.getDeclaredField("sender")
-//        // Is the sender field of the correct type?
-//        assertEquals(IOUState::class.java.getDeclaredField("sender").type, Party::class.java)
-//    }
-//
-//    @Test
-//    fun hasRecipientFieldOfCorrectType() {
-//        // Does the recipient field exist?
-//        IOUState::class.java.getDeclaredField("recipient")
-//        // Is the recipient field of the correct type?
-//        assertEquals(IOUState::class.java.getDeclaredField("recipient").type, Party::class.java)
-//    }
-//
-//    @Test
-//    fun senderIsParticipant() {
-//        val iouState = IOUState(1, miniCorp, megaCorp, IOUContract())
-//        assertNotEquals(iouState.participants.indexOf(miniCorpKey), -1)
-//    }
-//
-//    @Test
-//    fun recipientIsParticipant() {
-//        val iouState = IOUState(1, miniCorp, megaCorp, IOUContract())
-//        assertNotEquals(iouState.participants.indexOf(megaCorpKey), -1)
-//    }
-//
-//    @Test
-//    fun isLinearState() {
-//        assert(LinearState::class.java.isAssignableFrom(IOUState::class.java))
-//    }
-//
-//    @Test
-//    fun isRelevantToSender() {
-//        val iouState = IOUState(1, miniCorp, megaCorp, IOUContract())
-//        assert(iouState.isRelevant(setOf(miniCorpKey.singleKey)))
-//    }
-//
-//    @Test
-//    fun isRelevantToRecipient() {
-//        val iouState = IOUState(1, miniCorp, megaCorp, IOUContract())
-//        assert(iouState.isRelevant(setOf(megaCorpKey.singleKey)))
-//    }
+    @Test
+    fun hasIOUValueFieldOfCorrectType() {
+        // Does the iouValue field exist?
+        IOUState::class.java.getDeclaredField("iouValue")
+        // Is the iouValue field of the correct type?
+        assertEquals(IOUState::class.java.getDeclaredField("iouValue").type, Int::class.java)
+    }
+
+    @Test
+    fun hasSenderFieldOfCorrectType() {
+        // Does the sender field exist?
+        IOUState::class.java.getDeclaredField("sender")
+        // Is the sender field of the correct type?
+        assertEquals(IOUState::class.java.getDeclaredField("sender").type, Party::class.java)
+    }
+
+    @Test
+    fun hasRecipientFieldOfCorrectType() {
+        // Does the recipient field exist?
+        IOUState::class.java.getDeclaredField("recipient")
+        // Is the recipient field of the correct type?
+        assertEquals(IOUState::class.java.getDeclaredField("recipient").type, Party::class.java)
+    }
+
+    @Test
+    fun senderIsParticipant() {
+        val iouState = IOUState(1, miniCorp, megaCorp, IOUContract())
+        assertNotEquals(iouState.participants.indexOf(miniCorpKey), -1)
+    }
+
+    @Test
+    fun recipientIsParticipant() {
+        val iouState = IOUState(1, miniCorp, megaCorp, IOUContract())
+        assertNotEquals(iouState.participants.indexOf(megaCorpKey), -1)
+    }
+
+    @Test
+    fun isLinearState() {
+        assert(LinearState::class.java.isAssignableFrom(IOUState::class.java))
+    }
+
+    @Test
+    fun isRelevantToSender() {
+        val iouState = IOUState(1, miniCorp, megaCorp, IOUContract())
+        assert(iouState.isRelevant(setOf(miniCorpKey.singleKey)))
+    }
+
+    @Test
+    fun isRelevantToRecipient() {
+        val iouState = IOUState(1, miniCorp, megaCorp, IOUContract())
+        assert(iouState.isRelevant(setOf(megaCorpKey.singleKey)))
+    }
 }
