ad22fbde2116b72d8e9127fd5ec8f659b40884af
Tim Blankers <mail@timblankers.nl>
2017-04-04 20:51:36 +0100

    Add verification clauses and transaction tests

--- kotlin-source/src/main/kotlin/com/iou/kotlin/contract/IOUContract.kt
+++ kotlin-source/src/main/kotlin/com/iou/kotlin/contract/IOUContract.kt
@@ -1,10 +1,7 @@
 package com.iou.kotlin.contract
 
 import com.iou.kotlin.state.IOUState
-import net.corda.core.contracts.CommandData
-import net.corda.core.contracts.Contract
-import net.corda.core.contracts.TransactionForContract
-import net.corda.core.contracts.requireThat
+import net.corda.core.contracts.*
 import net.corda.core.crypto.SecureHash
 
 class IOUContract : Contract {
@@ -12,7 +9,20 @@ class IOUContract : Contract {
      * The verify() function of all the states' contracts must not throw an exception for a transaction to be
      * considered valid.
      */
-    override fun verify(tx: TransactionForContract) { return }
+    override fun verify(tx: TransactionForContract) {
+        tx.commands.requireSingleCommand<Commands.Create>()
+        requireThat {
+            "No inputs should be consumed when issuing an IOU." by (tx.inputs.isEmpty())
+            "Only one output state should be created." by (tx.outputs.size == 1)
+            val output = tx.outputs.single() as IOUState
+            "The IOU's value must be non-negative." by (output.iouValue > 0)
+            "All of the participants must be signers." by (tx.commands.single().signers.containsAll(output.participants))
+        }
+    }
+
+    interface Commands : CommandData {
+        class Create : TypeOnlyCommandData(), Commands
+    }
 
     /** This is a reference to the underlying legal contract template and associated parameters.  */
     override val legalContractReference = SecureHash.sha256("IOU contract template and params")
--- kotlin-source/src/test/kotlin/com/iou/kotlin/transactions/IOUTransactionTests.kt
+++ kotlin-source/src/test/kotlin/com/iou/kotlin/transactions/IOUTransactionTests.kt
@@ -7,70 +7,70 @@ import org.junit.Test
 
 class IOUTransactionTests {
 
-//    @Test
-//    fun mustIncludeCommand() {
-//        ledger {
-//            transaction {
-//                output { IOUState(1, MINI_CORP, MEGA_CORP, IOUContract()) }
-//                fails()
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun cannotCreateNegativeValueIOUs() {
-//        ledger {
-//            transaction {
-//                output(IOUState(-1, MINI_CORP, MEGA_CORP, IOUContract()))
-//                command(MINI_CORP_PUBKEY, MEGA_CORP_PUBKEY) { IOUContract.Create() }
-//                failsWith("The IOU's value must be non-negative.")
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun createTransactionMustHaveNoInputs() {
-//        ledger {
-//            transaction {
-//                input { IOUState(1, MINI_CORP, MEGA_CORP, IOUContract()) }
-//                output { IOUState(1, MINI_CORP, MEGA_CORP, IOUContract()) }
-//                command(MINI_CORP_PUBKEY, MEGA_CORP_PUBKEY) { IOUContract.Create() }
-//                failsWith("No inputs should be consumed when issuing an IOU.")
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun createTransactionMustHaveOneOutput() {
-//        ledger {
-//            transaction {
-//                output { IOUState(1, MINI_CORP, MEGA_CORP, IOUContract()) }
-//                output { IOUState(1, MINI_CORP, MEGA_CORP, IOUContract()) }
-//                command(MINI_CORP_PUBKEY, MEGA_CORP_PUBKEY) { IOUContract.Create() }
-//                failsWith("Only one output state should be created.")
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun senderMustSignCreateTransaction() {
-//        ledger {
-//            transaction {
-//                output { IOUState(1, MINI_CORP, MEGA_CORP, IOUContract()) }
-//                command(MEGA_CORP_PUBKEY) { IOUContract.Create() }
-//                failsWith("All of the participants must be signers.")
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun recipientMustSignCreateTransaction() {
-//        ledger {
-//            transaction {
-//                output { IOUState(1, MINI_CORP, MEGA_CORP, IOUContract()) }
-//                command(MINI_CORP_PUBKEY) { IOUContract.Create() }
-//                failsWith("All of the participants must be signers.")
-//            }
-//        }
-//    }
+    @Test
+    fun mustIncludeCommand() {
+        ledger {
+            transaction {
+                output { IOUState(1, MINI_CORP, MEGA_CORP, IOUContract()) }
+                fails()
+            }
+        }
+    }
+
+    @Test
+    fun cannotCreateNegativeValueIOUs() {
+        ledger {
+            transaction {
+                output(IOUState(-1, MINI_CORP, MEGA_CORP, IOUContract()))
+                command(MINI_CORP_PUBKEY, MEGA_CORP_PUBKEY) { IOUContract.Commands.Create() }
+                failsWith("The IOU's value must be non-negative.")
+            }
+        }
+    }
+
+    @Test
+    fun createTransactionMustHaveNoInputs() {
+        ledger {
+            transaction {
+                input { IOUState(1, MINI_CORP, MEGA_CORP, IOUContract()) }
+                output { IOUState(1, MINI_CORP, MEGA_CORP, IOUContract()) }
+                command(MINI_CORP_PUBKEY, MEGA_CORP_PUBKEY) { IOUContract.Commands.Create() }
+                failsWith("No inputs should be consumed when issuing an IOU.")
+            }
+        }
+    }
+
+    @Test
+    fun createTransactionMustHaveOneOutput() {
+        ledger {
+            transaction {
+                output { IOUState(1, MINI_CORP, MEGA_CORP, IOUContract()) }
+                output { IOUState(1, MINI_CORP, MEGA_CORP, IOUContract()) }
+                command(MINI_CORP_PUBKEY, MEGA_CORP_PUBKEY) { IOUContract.Commands.Create() }
+                failsWith("Only one output state should be created.")
+            }
+        }
+    }
+
+    @Test
+    fun senderMustSignCreateTransaction() {
+        ledger {
+            transaction {
+                output { IOUState(1, MINI_CORP, MEGA_CORP, IOUContract()) }
+                command(MEGA_CORP_PUBKEY) { IOUContract.Commands.Create() }
+                failsWith("All of the participants must be signers.")
+            }
+        }
+    }
+
+    @Test
+    fun recipientMustSignCreateTransaction() {
+        ledger {
+            transaction {
+                output { IOUState(1, MINI_CORP, MEGA_CORP, IOUContract()) }
+                command(MINI_CORP_PUBKEY) { IOUContract.Commands.Create() }
+                failsWith("All of the participants must be signers.")
+            }
+        }
+    }
 }
