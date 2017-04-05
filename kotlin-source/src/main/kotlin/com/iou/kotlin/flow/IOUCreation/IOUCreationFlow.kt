68d9132fbae8dda297c52a507aabce58e719b7b8
Tim Blankers <mail@timblankers.nl>
2017-04-05 15:20:58 +0100

    Add IOU creation flow and tests

--- kotlin-source/src/main/kotlin/com/iou/kotlin/flow/IOUCreation/IOUCreationFlow.kt
+++ kotlin-source/src/main/kotlin/com/iou/kotlin/flow/IOUCreation/IOUCreationFlow.kt
@@ -12,15 +12,52 @@ import net.corda.core.transactions.SignedTransaction
 import net.corda.flows.FinalityFlow
 
 object IOUCreationFlow {
-    class Initiator(val iou: IOUState, val otherParty: Party) : FlowLogic<Unit>() {
+    class Initiator(val iou: IOUState, val otherParty: Party) : FlowLogic<SignedTransaction>() {
 
         @Suspendable
-        override fun call() { }
+        override fun call(): SignedTransaction {
+            // Obtain a reference to the notary and parties we want to use.
+            val notary = serviceHub.networkMapCache.notaryNodes.single().notaryIdentity
+            val me = serviceHub.legalIdentityKey
+
+            // Stage 1 - Generating an unsigned transaction
+            val txCommand = Command(IOUContract.Commands.Create(), iou.participants)
+            val unsignedTx = TransactionType.General.Builder(notary).withItems(iou, txCommand)
+
+            // Stage 2 - Verifying the transaction
+            unsignedTx.toWireTransaction().toLedgerTransaction(serviceHub).verify()
+
+            // Stage 3 - Signing the transaction
+            val partSignedTx = unsignedTx.signWith(serviceHub.legalIdentityKey).toSignedTransaction(false)
+
+            val txFromCounterparty = sendAndReceive(SignedTransaction::class.java, otherParty, partSignedTx).unwrap { tx -> tx }
+
+            return txFromCounterparty
+        }
     }
 
     class Acceptor(val otherParty: Party) : FlowLogic<Unit>() {
 
         @Suspendable
-        override fun call() { }
+        override fun call() {
+            // Obtain a reference to the notary we want to use.
+            val me = serviceHub.legalIdentityKey
+            val notary = serviceHub.networkMapCache.notaryNodes.single().notaryIdentity
+
+            val inboundPartSignedTx = receive<SignedTransaction>(otherParty).unwrap {
+                tx ->
+                    val wireTx = tx.verifySignatures(me.public.composite, notary!!.owningKey)
+                    wireTx.toLedgerTransaction(serviceHub).verify()
+                tx
+            }
+
+            val mySignature = inboundPartSignedTx.signWithECDSA(me)
+            val counterpartySignedTxn = inboundPartSignedTx + mySignature
+
+            send(otherParty, counterpartySignedTxn)
+            val allParticipants = setOf(serviceHub.myInfo.legalIdentity, otherParty)
+            subFlow(FinalityFlow(counterpartySignedTxn, allParticipants))
+
+        }
     }
 }
--- kotlin-source/src/test/kotlin/com/iou/kotlin/flows/IOUFlowTests.kt
+++ kotlin-source/src/test/kotlin/com/iou/kotlin/flows/IOUFlowTests.kt
@@ -33,92 +33,92 @@ class IOUFlowTests {
         net.stopNodes()
     }
 
-//    @Test
-//    fun flowReturnsTransactionSignedByTheInitiator() {
-//        val state = IOUState(
-//                1,
-//                a.info.legalIdentity,
-//                b.info.legalIdentity,
-//                IOUContract())
-//        val flow = IOUFlow.Initiator(state, b.info.legalIdentity)
-//        val future = a.services.startFlow(flow).resultFuture
-//        net.runNetwork()
-//
-//        val signedTx: SignedTransaction = future.getOrThrow()
-//
-//        signedTx.verifySignatures(b.info.legalIdentity.owningKey, DUMMY_NOTARY.owningKey)
-//    }
+    @Test
+    fun flowReturnsTransactionSignedByTheInitiator() {
+        val state = IOUState(
+                1,
+                a.info.legalIdentity,
+                b.info.legalIdentity,
+                IOUContract())
+        val flow = IOUCreationFlow.Initiator(state, b.info.legalIdentity)
+        val future = a.services.startFlow(flow).resultFuture
+        net.runNetwork()
 
-//    @Test
-//    fun flowRejectsInvalidIOUStates() {
-//        // The IOUContract specifies that an IOU cannot have a negative value.
-//        val state = IOUState(
-//                -1,
-//                a.info.legalIdentity,
-//                b.info.legalIdentity,
-//                IOUContract())
-//        val flow = IOUFlow.Initiator(state, b.info.legalIdentity)
-//        val future = a.services.startFlow(flow).resultFuture
-//        net.runNetwork()
-//
-//        assertFailsWith<TransactionVerificationException> { future.getOrThrow() }
-//    }
-//
-//    @Test
-//    fun flowReturnsTransactionSignedByTheAcceptor() {
-//        val state = IOUState(
-//                1,
-//                a.info.legalIdentity,
-//                b.info.legalIdentity,
-//                IOUContract())
-//        val flow = IOUFlow.Initiator(state, b.info.legalIdentity)
-//        val future = a.services.startFlow(flow).resultFuture
-//        net.runNetwork()
-//
-//        val signedTx = future.getOrThrow()
-//        signedTx.verifySignatures(a.info.legalIdentity.owningKey, DUMMY_NOTARY.owningKey)
-//    }
-//
-//    @Test
-//    fun flowRecordsATransactionInBothPartiesVaults() {
-//        val state = IOUState(
-//                1,
-//                a.info.legalIdentity,
-//                b.info.legalIdentity,
-//                IOUContract())
-//        val flow = IOUFlow.Initiator(state, b.info.legalIdentity)
-//        val future = a.services.startFlow(flow).resultFuture
-//        net.runNetwork()
-//        val signedTx = future.getOrThrow()
-//
-//        for (node in listOf(a, b)) {
-//            val recordedTxA = node.storage.validatedTransactions.getTransaction(signedTx.id)
-//            assertEquals(signedTx.id, recordedTxA!!.id)
-//        }
-//    }
-//
-//    @Test
-//    fun recordedTransactionHasNoInputsAndASingleOutputTheInputIOU() {
-//        val inputState = IOUState(
-//                1,
-//                a.info.legalIdentity,
-//                b.info.legalIdentity,
-//                IOUContract())
-//        val flow = IOUFlow.Initiator(inputState, b.info.legalIdentity)
-//        val future = a.services.startFlow(flow).resultFuture
-//        net.runNetwork()
-//        val signedTx = future.getOrThrow()
-//
-//        for (node in listOf(a, b)) {
-//            val recordedTx = node.storage.validatedTransactions.getTransaction(signedTx.id)
-//            val txOutputs = recordedTx!!.tx.outputs
-//            assert(txOutputs.size == 1)
-//
-//            val recordedState = txOutputs.first().data as IOUState
-//            assertEquals(recordedState.iouValue, inputState.iouValue)
-//            assertEquals(recordedState.sender, inputState.sender)
-//            assertEquals(recordedState.recipient, inputState.recipient)
-//            assertEquals(recordedState.linearId, inputState.linearId)
-//        }
-//    }
+        val signedTx: SignedTransaction = future.getOrThrow()
+
+        signedTx.verifySignatures(a.info.legalIdentity.owningKey, DUMMY_NOTARY.owningKey)
+    }
+
+    @Test
+    fun flowRejectsInvalidIOUStates() {
+        // The IOUContract specifies that an IOU cannot have a negative value.
+        val state = IOUState(
+                -1,
+                a.info.legalIdentity,
+                b.info.legalIdentity,
+                IOUContract())
+        val flow = IOUCreationFlow.Initiator(state, b.info.legalIdentity)
+        val future = a.services.startFlow(flow).resultFuture
+        net.runNetwork()
+
+        assertFailsWith<TransactionVerificationException> { future.getOrThrow() }
+    }
+
+    @Test
+    fun flowReturnsTransactionSignedByTheAcceptor() {
+        val state = IOUState(
+                1,
+                a.info.legalIdentity,
+                b.info.legalIdentity,
+                IOUContract())
+        val flow = IOUCreationFlow.Initiator(state, b.info.legalIdentity)
+        val future = a.services.startFlow(flow).resultFuture
+        net.runNetwork()
+
+        val signedTx = future.getOrThrow()
+        signedTx.verifySignatures(a.info.legalIdentity.owningKey, DUMMY_NOTARY.owningKey)
+    }
+
+    @Test
+    fun flowRecordsATransactionInBothPartiesVaults() {
+        val state = IOUState(
+                1,
+                a.info.legalIdentity,
+                b.info.legalIdentity,
+                IOUContract())
+        val flow = IOUCreationFlow.Initiator(state, b.info.legalIdentity)
+        val future = a.services.startFlow(flow).resultFuture
+        net.runNetwork()
+        val signedTx = future.getOrThrow()
+
+        for (node in listOf(a, b)) {
+            val recordedTxA = node.storage.validatedTransactions.getTransaction(signedTx.id)
+            assertEquals(signedTx.id, recordedTxA!!.id)
+        }
+    }
+
+    @Test
+    fun recordedTransactionHasNoInputsAndASingleOutputTheInputIOU() {
+        val inputState = IOUState(
+                1,
+                a.info.legalIdentity,
+                b.info.legalIdentity,
+                IOUContract())
+        val flow = IOUCreationFlow.Initiator(inputState, b.info.legalIdentity)
+        val future = a.services.startFlow(flow).resultFuture
+        net.runNetwork()
+        val signedTx = future.getOrThrow()
+
+        for (node in listOf(a, b)) {
+            val recordedTx = node.storage.validatedTransactions.getTransaction(signedTx.id)
+            val txOutputs = recordedTx!!.tx.outputs
+            assert(txOutputs.size == 1)
+
+            val recordedState = txOutputs.first().data as IOUState
+            assertEquals(recordedState.iouValue, inputState.iouValue)
+            assertEquals(recordedState.sender, inputState.sender)
+            assertEquals(recordedState.recipient, inputState.recipient)
+            assertEquals(recordedState.linearId, inputState.linearId)
+        }
+    }
 }
