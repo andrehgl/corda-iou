59f4d4423e51713d8c629f4d5d791ec1bf67c40b
Tim Blankers <mail@timblankers.nl>
2017-04-05 10:25:16 +0100

    Add transfer clauses and tests

--- kotlin-source/src/main/kotlin/com/iou/kotlin/contract/IOUContract.kt
+++ kotlin-source/src/main/kotlin/com/iou/kotlin/contract/IOUContract.kt
@@ -10,18 +10,44 @@ class IOUContract : Contract {
      * considered valid.
      */
     override fun verify(tx: TransactionForContract) {
-        tx.commands.requireSingleCommand<Commands.Create>()
+//        tx.commands.requireSingleCommand<Commands.Create>()
+
         requireThat {
-            "No inputs should be consumed when issuing an IOU." by (tx.inputs.isEmpty())
-            "Only one output state should be created." by (tx.outputs.size == 1)
-            val output = tx.outputs.single() as IOUState
-            "The IOU's value must be non-negative." by (output.iouValue > 0)
-            "All of the participants must be signers." by (tx.commands.single().signers.containsAll(output.participants))
+            "Command should be included" by (tx.commands.isNotEmpty())
         }
+
+        tx.commands.forEach {
+            val commandData = it.value
+            if (commandData is Commands.Create) {
+                requireThat {
+                    "No inputs should be consumed when issuing an IOU." by (tx.inputs.isEmpty())
+                    "Only one output state should be created." by (tx.outputs.size == 1)
+                    val output = tx.outputs.single() as IOUState
+                    "The IOU's value must be non-negative." by (output.iouValue > 0)
+                    "All of the participants must be signers." by (tx.commands.single().signers.containsAll(output.participants))
+                }
+            }
+            else if (commandData is Commands.Transfer) {
+                requireThat {
+                    "One input should be consumed when transferring an IOU." by (tx.inputs.size == 1)
+                    "Failed requirement: Only one output state should be created." by (tx.outputs.size == 1)
+                    val output = tx.outputs.single() as IOUState
+                    val input = tx.inputs.single() as IOUState
+                    "The IOU's value must be non-negative." by (output.iouValue > 0)
+                    "The input and output IOUs must have the same value." by (input.iouValue == output.iouValue)
+                    "The input and output IOUs must have the same sender." by (input.sender == output.sender)
+                    "The input and output IOUs must have different recipients." by (input.recipient != output.recipient)
+                    "All of the participants must be signers." by (it.signers.containsAll(tx.inputs.single().participants))
+                    "All of the participants must be signers." by (it.signers.containsAll(tx.outputs.single().participants))
+                }
+            }
+        }
+
     }
 
     interface Commands : CommandData {
         class Create : TypeOnlyCommandData(), Commands
+        class Transfer : TypeOnlyCommandData(), Commands
     }
 
     /** This is a reference to the underlying legal contract template and associated parameters.  */
